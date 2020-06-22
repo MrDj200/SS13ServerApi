26ac83899ee3bec605e65c08103bbb7967988182
MrDj200 <mrdj201@gmail.com>
2020-06-22 15:35:28 +0200

    Added Root Controller

---
+++ SS13ServerApi/Controllers/RootController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+
+namespace SS13ServerApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        [OpenApiIgnore]
+        public ActionResult Get() => Redirect("/swagger");
+    }
+}
