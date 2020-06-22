5d496066e18555b1dfab212b2cd2432d8786b2da
MrDj200 <mrdj201@gmail.com>
2020-06-22 15:35:17 +0200

    Cleanup

--- SS13ServerApi/Controllers/SS13Controller.cs
+++ SS13ServerApi/Controllers/SS13Controller.cs
@@ -1,5 +1,6 @@
 ﻿using ByondTopic;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,10 @@ namespace SS13ServerApi.Controllers
     [ApiController]
     public class SS13Controller : ControllerBase
     {
+        [HttpGet]
+        [OpenApiIgnore]
+        public ActionResult Get() => Redirect("/swagger/index.html#/SS13");
+
         [HttpGet("status")]
         public ActionResult GetServerInfo([FromQuery] string address = "whipit.de", [FromQuery] ushort port = 1337)
         {
@@ -17,13 +22,13 @@ namespace SS13ServerApi.Controllers
             {
                 TopicSource topic = new TopicSource(address, port);
                 QueryResponse queryResponse = topic.Query("status");
-                return Ok(queryResponse.ProperJson());
+                return Ok(new SS13Status(queryResponse));
             }
             catch (System.Net.Sockets.SocketException)
             {
                 return BadRequest(new
                 {
-                    Title = "One ore more validation errors occured",
+                    Title = "One or more validation errors occured",
                     Status = 400,
                     Errors = new Dictionary<string, string[]> { { "address", new[] { $"The value '{address}' is not valid" } } }
                 });
