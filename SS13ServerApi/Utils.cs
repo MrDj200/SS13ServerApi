27a38818640e5be6a6cba029cc409d61de6ceeee
MrDj200 <mrdj201@gmail.com>
2020-06-23 23:29:54 +0200

    Some Comments
    Mainly to test jenkins

--- SS13ServerApi/Utils.cs
+++ SS13ServerApi/Utils.cs
@@ -8,6 +8,7 @@ namespace SS13ServerApi
     {
         public static async Task<bool> IsValidAddress(string address)
         {
+            // Checks if the given IP is a valid IP and is not a loopback
             return !(await Dns.GetHostAddressesAsync(address)).Any(IP => IPAddress.IsLoopback(IP));
         }
     }
