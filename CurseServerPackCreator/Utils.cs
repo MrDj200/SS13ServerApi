985471bac0284dfa2fc161fd36102a9f41d421d2
MrDj200 <mrdj201@gmail.com>
2020-10-22 09:19:50 +0200

    Started ChangeMySkin

---
+++ CurseServerPackCreator/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurseServerPackCreator
+{
+    public static class Utils
+    {
+        public static async Task<string> MakeGetRequest(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using Stream stream = response.GetResponseStream();
+            using StreamReader reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
---
+++ SS13ServerApi/ChangeMySkin/Responses/AuthenticateResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SS13ServerApi.ChangeMySkin.Responses
+{
+    internal class AuthenticateResponse : Response
+    {
+        /// <summary>
+        /// Access Token for this user
+        /// </summary>
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; internal set; }
+
+        /// <summary>
+        /// Must be the same as Requester.ClientToken
+        /// </summary>
+        [JsonProperty("clientToken")]
+        public string ClientToken { get; internal set; }
+    }
+}
